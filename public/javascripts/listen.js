d2bd58087edeb0c45af2e3695604ac40341f63d3
SuddenDevelopment <Anthony.Aragues@gmail.com>
2018-03-27 20:41:13 -0400

    moved dom lookup of status id to a var and styles for live or dead status

--- public/javascripts/listen.js
+++ public/javascripts/listen.js
@@ -4,6 +4,7 @@
 // TODO: we can establish stadards for the kind of JS permitted in the project. _This_ page requires chrome and could use most ES6+ features that are implemented in Chrome, but the observer page is targeted to any possible display that can run enough JS for firebase.
 // TODO: Transcripts should be really simple to use and broadcast. But right now there is just read/write access to the DB for anyody who wants it. I would like to have no login whatsoever and still have the person who started a listening session ALWAYS remain in charge of it, so it couldn't be over written. There are probably many ways to do it. Right now if I am recording a named transcript, and somebody else names their transcript the same name as mine, we are both then feeding into the same exact DB location. That's dumb. But there are cases where you WOULD want to resume an existing named transcript at right now you just type in the name and pick up where you left off. Really curious to see how we can make this "just work" by maybe caching something in a cookie or local storage or whatever.
 // TODO: I haven't dug into Zenscroll enough to know how it handles being called repeatedly, but sometimes I see jankyness and we may need to look at how to only call Zenscroll if we are not already scrolling. My naive version of that didn't work as expected.
+
 var transcript = document.querySelector('#transcript')
 var transcriptID = ''
 var snippetIDs = []
@@ -186,7 +187,10 @@ function toggle() {
 
 	listening = !listening
 
-	document.querySelector('#status').textContent = 'Listening'
+	var domStatus = document.querySelector('#status');
+	domStatus.textContent = 'Listening';
+	domStatus.setAttribute('class','live');
+
 	Array.from(document.getElementsByClassName('startListen')).map(function(
 		element
 	) {
@@ -200,10 +204,12 @@ function toggle() {
 	if (!listening) {
 		recognition.stop()
 		interim.textContent = ''
-		document.querySelector('#status').textContent = 'Not Listening'
+		domStatus.textContent = 'Not Listening';
+		domStatus.setAttribute('class','dead');
 	} else {
 		recognition.start()
-		document.querySelector('#status').textContent = 'Listening'
+		domStatus.textContent = 'Listening';
+		domStatus.setAttribute('class','live');
 	}
 }
 
@@ -323,11 +329,3 @@ function completion(error) {
 		console.log('Data saved successfully.')
 	}
 }
-
-// List of supported languages from Web Speech API
- const returnLanguageCode = (selectedLanguage) => {
-	const correctLanguage = languageList.filter(language => language[0] === selectedLanguage)
-	return correctLanguage[0][1][0]
-}
-
-console.log(returnLanguageCode('Italiano'))
