58f5755433a6664a3af7ce203d33fd9d3bba942c
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-10-09 21:50:04 -0400

    Revert "started working on a button that toggles between show header and hide header and is styled the same as the other buttons. initial commit work in progress."
    This reverts commit 8e5919b1091e2d9b75530e4f0c5170d0fd415f8c.

--- javascripts/listen.js
+++ javascripts/listen.js
@@ -392,7 +392,7 @@ function init() {
     .addEventListener("click", aboutToggle)
 
   document
-    .querySelector(".toggle-button")
+    .querySelector("#header-toggle-button")
     .addEventListener("click", toggleHeader);
 
   document
