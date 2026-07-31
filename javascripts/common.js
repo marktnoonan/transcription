219c46d788fa32b73f4e6e88c16b506cab238ddf
anupamme <mediratta@gmail.com>
2026-07-31 07:11:52 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,10 +1,10 @@
 var config = {
-	apiKey: "AIzaSyD591s8Of9vqduC2ZAakt9mMQRin4wCFSQ",
-	authDomain: "live-transcript.firebaseapp.com",
-	databaseURL: "https://live-transcript.firebaseio.com",
-	projectId: "live-transcript",
-	storageBucket: "",
-	messagingSenderId: "1024703297861"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 var darkThemeOn = false;
